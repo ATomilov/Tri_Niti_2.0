dbdbc850a78ddb0ab4513f2d43c1c44604913049
ATomilov <tomilovlehach@mail.ru>
2017-04-23 18:57:32 +0600

    Заменен курсор в режиме фигур на более полигональный

--- ТриНитиДизайн/obj/x86/Debug/ТриНитиДизайн_Content.g.i.cs
+++ ТриНитиДизайн/obj/x86/Debug/ТриНитиДизайн_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("cursors/arrowinlineregim.cur")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("cursors/sword.cur")]
 
 
