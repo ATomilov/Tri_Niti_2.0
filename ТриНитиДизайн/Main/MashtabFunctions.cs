c476761e661e2afb5490e244cacbed951575787c
gibsman <gibsman319@gmail.com>
2018-03-18 23:29:49 +0600

    Update gitignore (final)

--- ТриНитиДизайн/Main/MashtabFunctions.cs
+++ ТриНитиДизайн/Main/MashtabFunctions.cs
@@ -173,7 +173,6 @@ namespace ТриНитиДизайн
             {
                 GeometryHelper.RescaleRectangle(rec, multiplier);
             }
-            //
             foreach (Ellipse ell in fig.dotsForFigure)
                 GeometryHelper.RescaleEllipse(ell, multiplier);
             if(fig.NewPointEllipse != null)
